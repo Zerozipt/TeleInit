7982967612ecf0fe46ac7c0eb2ff42146ad4e236
ZerozipsCmp <2385397431@qq.com>
2025-03-02 15:36:29 +0800

    最最基础前后端文件

---
+++ the-back/src/main/java/com/example/TheBackApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheBackApplication.class, args);
+    }
+
+}
