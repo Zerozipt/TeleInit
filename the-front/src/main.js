6e89f16e2ad498b72a1a5ec7bb8f49c562c89b8a
ZerozipsCmp <2385397431@qq.com>
2025-03-14 00:17:03 +0800

    用户登陆退出功能初实现

--- the-front/src/main.js
+++ the-front/src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import router from "@/router"
+import axios from 'axios'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = 'http://localhost:8080'
+
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
